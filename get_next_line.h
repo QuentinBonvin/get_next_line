4e879288bf5ded461a2108840ed4f58f055ec7a9
QuentinBonvin <qbonvin@student.42lausanne.ch>
2022-02-09 09:37:13 +0100

    correction du .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,17 @@
 /*   By: qbonvin <qbonvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 16:34:08 by qbonvin           #+#    #+#             */
-/*   Updated: 2022/02/09 09:19:11 by qbonvin          ###   ########.fr       */
+/*   Updated: 2022/02/09 09:35:48 by qbonvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
-
-#ifndef	GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <ctype.h>
@@ -26,12 +27,11 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
-int     ft_strlen(const char *str);
+int		ft_strlen(const char *str);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
-char    *ft_read_and_save(int fd, char *save);
-char    *ft_get_line(char *save);
+char	*ft_read_and_save(int fd, char *save);
+char	*ft_get_line(char *save);
 char	*ft_save(char *save);
 
 #endif
-
